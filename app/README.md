5ddceb6afef939cf891751502fc514d23499fe89
Marcio Antonio Gonçalves dos Santos <contato@3wbrasilprojetoswebsites.com.br>
2021-11-11 08:54:15 -0300

    primeiro commit

---
+++ app/README.md
@@ -0,0 +1,24 @@
+SEQUENCIA PARA CRIAR O PROJETO
+Instalar o Expo global no pront de comando
+### npm install -g expo-cli
+
+Criar o projeto com React Native
+### expo init NOME_DO_PROJETO
+
+Rodar o projeto usando Expo
+### npm start
+
+Para navegar entre as páginas
+### npm install @react-navigation/native
+### expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context
+### npm install @react-navigation/stack react-native-gesture-handler
+
+Transforma o CSS em componentes
+### npm install --save styled-components
+
+Realizar chamada para API
+### npm install --save axios
+
+Criar gráficos
+### expo install react-native-svg
+### npm install --save react-native-svg-charts
---
+++ app/src/pages/MenorEstoque/index.js
@@ -0,0 +1,73 @@
+import React, { useCallback, useState } from 'react';
+import {useFocusEffect} from '@react-navigation/native';
+
+import {PieChart} from 'react-native-svg-charts';
+import {Text} from 'react-native-svg';
+
+import { FlatList, Text as Text2} from 'react-native';
+import {Container} from './styles';
+
+import api from '../../config/configApi';
+
+export default function MenorEstoque(){
+
+    const [data, setData] = useState([]);
+
+    const getProdutos = async () => {
+        const response = await api.get('/grafico-menor-estoque');
+        setData(response.data.produtos);
+    }
+
+    const pieData = data.map((produto) => ({
+        value: produto.quantidade,
+        key: produto.id,
+        svg: {
+            fill: '#007281'
+        }
+    }));
+
+    const Label = ({ slices }) => {
+        return slices.map((slice, index) => {
+            const { pieCentroid, data } = slice;
+
+            return(
+                <Text
+                key={data.key}
+                x={pieCentroid[0]}
+                y={pieCentroid[1]}
+                fill="#f2b705"
+                textAnchor={'middle'}
+                alignmentBaseline={'middle'}
+                fontSize={16}
+                >
+                    {data.key}
+                </Text>
+            )
+        })
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            getProdutos();
+        }, [])
+    );
+
+    return(
+        <Container>
+            <PieChart style={{height: 300}} data={pieData}>
+                <Label />
+            </PieChart>
+
+            <FlatList 
+                data={data}
+                renderItem={({item}) => (
+                    <>
+                        <Text2>
+                            {item.id} - {item.nome} - {item.quantidade}
+                        </Text2>
+                    </>
+                )} keyExtractor={produto => String(produto.id)}
+            />
+        </Container>
+    )
+}
